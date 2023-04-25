0c129189d428f2fa8af2aff0aba4c7d5b9dca07a
SIU-JANG <qq6988@naver.com>
2023-04-25 14:20:19 +0900

    Add: app

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const HOST = "http://k8e202.p.ssafy.io"
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on ${hostName}:${PORT}`);
+});
