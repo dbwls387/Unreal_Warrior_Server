1eddc9cef64df3f55c4121ca3d7ae31233d104be
SIU-JANG <qq6988@naver.com>
2023-04-27 12:03:17 +0900

    Fix : app

--- back/app.js
+++ back/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const fs = require('fs');
-const https = require('https');
-const app = express();
+const app = require('express')();
+// const fs = require('fs');
+// const https = require('https');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const PORT = 8080;
 
-const option = {
-    ca: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/fullchain.pem'),
-    // key: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/privkey.pem'),
-    // cert: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/cert.pem')
-};
+app.get('/', function(req, res) {
+    res.send('Hello World');
+})
 
-const server = https.createServer(option, app);
+// const option = {
+//     ca: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/fullchain.pem'),
+//     key: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/privkey.pem'),
+//     cert: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/cert.pem')
+// };
 
-server.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server running on https://k8e202.p.ssafy.io:${PORT}`);
 });
