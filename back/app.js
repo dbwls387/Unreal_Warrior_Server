567c61687a9d316d3c0fd705d9d69a840e59a917
SIU-JANG <qq6988@naver.com>
2023-04-27 10:33:44 +0900

    Fix : socket.io

--- back/app.js
+++ back/app.js
@@ -1,5 +1,5 @@
 const express = require('express')();
-// const io = require('socket.io');
+const io = require('socket.io');
 
 const PORT = 8080;
 
@@ -7,9 +7,9 @@ app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
-// io.on('connection', function(socket) {
-//     console.log('CONNECT');
-// });
+io.on('connection', function(socket) {
+    console.log('CONNECT');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://k8e202.p.ssafy.io:${PORT}`);
