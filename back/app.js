05a563ddf84df14974af09eadf31e2392cb6cccf
SIU-JANG <qq6988@naver.com>
2023-04-27 15:40:29 +0900

    Fix : socket.io

--- back/app.js
+++ back/app.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.redirect('https://k8e202.p.ssafy.io');
 });
 
 io.on('connection', (socket) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import io from 'socket.io-client';
+import { io } from "socket.io-client";
 
 function App() {	
+
+    var socket = io();
+
+      console.log(socket);
+
+      var messages = document.getElementById('messages');
+      var form = document.getElementById('form');
+      var input = document.getElementById('input');
+
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (input.value) {
+          socket.emit('chat message', input.value);
+          input.value = '';
+        }
+      });
+
+      socket.on('chat message', function(msg) {
+        var item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
   return (
     <div className="App">
+      <ul id="messages"></ul>
+      <form id="form" action="">
+        <input id="input" autocomplete="off" /><button>Send</button>
+      </form>
+
     </div>
   );
 }
