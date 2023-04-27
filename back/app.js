18c9b0129c9e6daeb281d2427b7c9d9190d48fe7
SIU-JANG <qq6988@naver.com>
2023-04-27 11:01:50 +0900

    Add; ws

--- back/app.js
+++ back/app.js
@@ -1,25 +1,29 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
 const app = express();
 
-const options = {
-    key: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/fullchain.pem'),
-    cert: fs.readFileSync('/var/jenkins_home/workspace/deploy/sslkey/privkey.pem')
-};
-
-const server = https.createServer(options, app);
+const ws = require('ws');
 
 const PORT = 8080;
 
-server.get('/', function(req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World');
 });
+
+const wsSocketServer = new ws.Server(
+    {
+        server: 'https://k8e202.p.ssafy.io',
+        port: 8080
+    }
+);
 
 // io.on('connection', function(socket) {
 //     console.log('CONNECT');
 // });
 
-server.listen(PORT, () => {
-    console.log(`Server running on http://k8e202.p.ssafy.io:${PORT}`);
+wsSocketServer.on('connection', (ws, request) => {
+    console.log('CONNECTED');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on https://k8e202.p.ssafy.io:${PORT}`);
 });
