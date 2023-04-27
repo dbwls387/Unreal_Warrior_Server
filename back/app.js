07305db69ea77e2d7eadc0260b5568c4856a2524
SIU-JANG <qq6988@naver.com>
2023-04-27 10:23:10 +0900

    Fix : socket.io

--- back/app.js
+++ back/app.js
@@ -1,13 +1,9 @@
-const express = require('express');
-
-const app = express();
-const PORT = 8080;
-
-const server = require('https').createServer(app);
+const app = require('express')();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.get("/", (req, res) => {
-    res.send('Hello World');
+app.get('/', function(req, res) {
+  res.send('Hello World');
 });
 
 io.on('connection', function(socket) {
