b9f1861316f3a33ee6b7ba3a63ca0aeaa80295fe
SIU-JANG <qq6988@naver.com>
2023-05-01 11:24:47 +0900

    Add : tensorflow.js

--- back/app.js
+++ back/app.js
@@ -3,6 +3,14 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 8080;
 
+const { loadGraphModel } = require("@tensorflow/tfjs");
+const tf = require('@tensorflow/tfjs');
+
+app.get("/model", async (req, res) => {
+  const model = await tf.loadLayersModel('model.json');
+  model.predict(tf.tensor3d([[0.5, 0.5, 0.5]])).print();
+});
+
 app.get("/", (req, res) => {
     res.redirect("https://k8e202.p.ssafy.io");
 });
