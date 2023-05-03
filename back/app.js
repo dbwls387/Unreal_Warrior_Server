609e31db5ee9828f95f0e62a934ab4e5468aff10
dbwls387 <dbwls387@naver.com>
2023-05-03 14:13:43 +0900

    Test: tensorflow? nginx?

--- back/app.js
+++ back/app.js
@@ -3,41 +3,41 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 8080;
 
-// const tf = require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs-node");
 const path = require("path");
 
-// async function loadModel() {
-//     console.error("여기 문제 발생하는듯");
-//     const model = await tf.loadLayersModel(
-//         "file://" + path.join(__dirname, "model.json")
-//     );
-//     const inputData = tf.tensor3d([
-//         [
-//             [
-//                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-//                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-//                 1, 1, 1, 1, 1, 1,
-//             ],
-//         ],
-//     ]);
-//     const prediction = model.predict(inputData);
+async function loadModel() {
+    console.error("여기 문제 발생하는듯");
+    const model = await tf.loadLayersModel(
+        "file://" + path.join(__dirname, "model.json")
+    );
+    const inputData = tf.tensor3d([
+        [
+            [
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1,
+            ],
+        ],
+    ]);
+    const prediction = model.predict(inputData);
 
-//     const result = await prediction.array();
-//     console.error("dtdat", result[0]);
-//     return result[0];
-// }
+    const result = await prediction.array();
+    console.error("dtdat", result[0]);
+    return result[0];
+}
 
-// app.get("/model", (req, res) => {
-//     console.error("ㅇㅅㅇ");
-//     loadModel().then((result) => {
-//         res.send(result);
-//     });
-// });
+app.get("/model", (req, res) => {
+    console.error("ㅇㅅㅇ");
+    loadModel().then((result) => {
+        res.send(result);
+    });
+});
 
-// app.get("/", (req, res) => {
-//     res.redirect("https://k8e202.p.ssafy.io");
-//     console.error("이거왜안대");
-// });
+app.get("/", (req, res) => {
+    res.redirect("https://k8e202.p.ssafy.io");
+    console.error("이거왜안대");
+});
 
 io.on("connection", (socket) => {
     socket.on("chat message", (msg) => {
