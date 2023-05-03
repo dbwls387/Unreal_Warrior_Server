d00e91b49e2512e34c438528fb8106da09e24937
dbwls387 <dbwls387@naver.com>
2023-05-03 11:00:06 +0900

    Add: socket sim_control

--- back/app.js
+++ back/app.js
@@ -3,29 +3,37 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 8080;
 
-const tf = require('@tensorflow/tfjs-node');
-const path = require('path');
+const tf = require("@tensorflow/tfjs-node");
+const path = require("path");
 
 async function loadModel() {
-  console.error("여기 문제 발생하는듯");
-  const model = await tf.loadLayersModel('file://' + path.join(__dirname, 'model.json'));
-  const inputData = tf.tensor3d([[
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
- ]]);
- const prediction = model.predict(inputData);
+    console.error("여기 문제 발생하는듯");
+    const model = await tf.loadLayersModel(
+        "file://" + path.join(__dirname, "model.json")
+    );
+    const inputData = tf.tensor3d([
+        [
+            [
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1,
+            ],
+        ],
+    ]);
+    const prediction = model.predict(inputData);
 
- const result = await prediction.array();
- console.error("dtdat", result[0]);
- return result[0];
+    const result = await prediction.array();
+    console.error("dtdat", result[0]);
+    return result[0];
 }
 
 app.get("/model", (req, res) => {
-//   loadModel();
-  console.error("ㅇㅅㅇ"); 
-//   loadModel().then(result => {
-//   res.send(result);
-//   });
-  res.send("안댄다...");
+    //   loadModel();
+    console.error("ㅇㅅㅇ");
+    //   loadModel().then(result => {
+    //   res.send(result);
+    //   });
+    res.send("안댄다...");
 });
 
 app.get("/", (req, res) => {
@@ -43,6 +51,11 @@ io.on("connection", (socket) => {
     socket.on("actor_status", (msg) => {
         io.emit("actor_status", msg);
         console.log("엠에스지", msg);
+    });
+
+    socket.on("sim_control", () => {
+        io.emit("sim_control");
+        console.log(msg);
     });
 });
 
