236b9e958e3c398df8aab32ea6610803c36a51a9
dbwls387 <dbwls387@naver.com>
2023-05-04 13:54:16 +0900

    Fix: text

--- frontend/src/Components/GameStatusComponent.js
+++ frontend/src/Components/GameStatusComponent.js
@@ -21,7 +21,7 @@ export default function GameStatusComponent() {
 	return (
 		<div className="h-28 p-2 w-full">
 			<div className="float-left m-4 ml-10 font-bold">
-				<div>서버 연결 상태 : 연결완료</div>
+				<div>게임 연결 상태 : 연결완료</div>
 			</div>
 
 			<div className="float-right w-60 mr-4">
