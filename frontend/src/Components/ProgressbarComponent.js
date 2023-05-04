52fc12f6ecc5ac9b35295c53db13738381371881
dbwls387 <dbwls387@naver.com>
2023-05-04 17:52:16 +0900

    Add: win_rate socket on

--- frontend/src/Components/ProgressbarComponent.js
+++ frontend/src/Components/ProgressbarComponent.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 export default function ProgressbarComponent() {
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		let copy = [...list];
+
+		const socket = io("https://k8e202.p.ssafy.io", {
+			path: "/socket.io",
+			// transports: ["websocket"],
+		});
+
+		socket.on("win_rate", data => {
+			copy = data;
+			setList(copy);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(list);
+	}, [list]);
+
 	return (
-		<div className="progress w-full h-24 bg-slate-200">프로그레스 바...</div>
+		<div className="progress w-full h-24 bg-slate-200">
+			승률 progress bar로 만들기...
+			<div>패: {list[0]}</div>
+			<div>승: {list[1]}</div>
+			<div>무: {list[2]}</div>
+		</div>
 	);
 }
