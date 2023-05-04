0cc47440290a5df89aa326b604796dbd9c5edf1d
dbwls387 <dbwls387@naver.com>
2023-05-04 12:19:46 +0900

    Fix: sim state socket

--- frontend/src/Components/GameStatusComponent.js
+++ frontend/src/Components/GameStatusComponent.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { io } from 'socket.io-client';
-import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import StopCircleOutlinedIcon from '@mui/icons-material/StopCircleOutlined';
+import React from "react";
+import { io } from "socket.io-client";
+import PauseCircleOutlineIcon from "@mui/icons-material/PauseCircleOutline";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import StopCircleOutlinedIcon from "@mui/icons-material/StopCircleOutlined";
 
 export default function GameStatusComponent() {
-	const onSocket = () => {
-		const socket = io('https://k8e202.p.ssafy.io', {
-			path: '/socket.io',
+	const onSocket = state => {
+		console.log(state);
+
+		const socket = io("https://k8e202.p.ssafy.io", {
+			path: "/socket.io",
 			// transports: ["websocket"],
 		});
 
-		socket.emit('sim_control', 'pause');
+		socket.emit("sim_control", state);
 
-		socket.on('hi', data => console.log(data)); // 서버 -> 클라이언트
+		// socket.on("hi", data => console.log(data)); // 서버 -> 클라이언트
 	};
 
 	return (
@@ -25,19 +27,19 @@ export default function GameStatusComponent() {
 			<div className="float-right w-60 mr-4">
 				<div
 					className="float-left w-1/3 text-center cursor-pointer"
-					onClick={onSocket}
+					onClick={onSocket("pause")}
 				>
 					<PauseCircleOutlineIcon sx={{ fontSize: 50 }} />
 				</div>
 				<div
 					className="float-left w-1/3 text-center cursor-pointer"
-					onClick={onSocket}
+					onClick={onSocket("play")}
 				>
 					<PlayCircleOutlineIcon sx={{ fontSize: 50 }} />
 				</div>
 				<div
 					className="float-left w-1/3 text-center cursor-pointer"
-					onClick={onSocket}
+					onClick={onSocket("stop")}
 				>
 					<StopCircleOutlinedIcon sx={{ fontSize: 50 }} />
 				</div>
