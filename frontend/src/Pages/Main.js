c158527f78507180d774d0f15298d93c845787ea
HYEJI KIM <pos04118@gmail.com>
2023-05-10 15:34:41 +0900

    blob test

--- frontend/src/Pages/Main.js
+++ frontend/src/Pages/Main.js
@@ -5,10 +5,20 @@ import ProgressbarComponent from "../Components/ProgressbarComponent";
 import UnrealComponent from "../Components/UnrealComponent";
 import { useParams } from "react-router-dom";
 import PlayerDetail from "../Components/PlayerDetail";
+import FileSaver from "file-saver"
+import unreal from "../unreal/aiAvatarTest.exe"
 
 export default function Main() {
 	const { playerId } = useParams();
 
+	
+	const downLoadUnreal = async (e) => {
+		let filename = 'aiAvatarTest.exe'
+		// let unrealBlob = exe(unreal).toBlob()
+		// let exe = new Blob(unreal, {type})
+		const exe = new Blob([unreal])
+		FileSaver.saveAs(exe, filename)
+	}
 	return (
 		<div className="main">
 			<div className="left">
@@ -19,7 +29,8 @@ export default function Main() {
 				{playerId != undefined && <PlayerDetail playerId={playerId} />}
 			</div>
 
-			<div className="right">
+			<div className="right" >
+				<button className="download" onClick={downLoadUnreal}>DownLoad</button>
 				<UnrealComponent />
 			</div>
 		</div>
