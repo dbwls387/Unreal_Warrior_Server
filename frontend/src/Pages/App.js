3a1b16e9e730710bc260fd99ef181a11690f05ce
dbwls387 <dbwls387@naver.com>
2023-05-10 17:53:06 +0900

    Fix: command

--- frontend/src/Pages/App.js
+++ frontend/src/Pages/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { io } from "socket.io-client";
 
 export default function App() {
-    const onSocket = () => {
-        const socket = io("https://k8e202.p.ssafy.io", {
-            path: "/socket.io",
-            // transports: ["websocket"],
-        });
-
-        socket.emit("sim_control", "pause");
-
-        socket.on("hi", (data) => console.log(data)); // 서버 -> 클라이언트
-    };
-
-    return <button onClick={onSocket}>일시정지</button>;
+	// const onSocket = () => {
+	//     const socket = io("https://k8e202.p.ssafy.io", {
+	//         path: "/socket.io",
+	//         // transports: ["websocket"],
+	//     });
+	//     socket.emit("sim_control", "pause");
+	//     socket.on("hi", (data) => console.log(data)); // 서버 -> 클라이언트
+	// };
+	// return <button onClick={onSocket}>일시정지</button>;
 }
