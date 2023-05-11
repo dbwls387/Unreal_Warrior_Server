ccc0a923aaa42b48cffb9ff996f6c31e2ca0495f
SIU-JANG <qq6988@naver.com>
2023-05-11 15:17:05 +0900

    Fix

--- back/app.js
+++ back/app.js
@@ -111,7 +111,7 @@ io.on("connection", socket => {
                     // socket.broadcast.emit("win_rate", result);
                     // socket.broadcast.emit("direction", results);
 
-                    console.log(result2.length);
+                    console.log(result2["indi"].length);
                 }
             } else {
                 console.error("playerNumber가 잘못함");
