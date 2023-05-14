0699366b8a7f67f047bf5a252eb22963f946c1a1
dbwls387 <dbwls387@naver.com>
2023-05-15 06:07:03 +0900

    Fix: react landing

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import Landing from "../Pages/Landing";
 import Main from "../Pages/Main";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <Main />,
+		// element: <Main />,
+		element: <Landing />,
 		// errorElement: <NotFound />,
+	},
+	{
+		path: "/",
+		children: [
+			{
+				path: "",
+				// 언리얼 화면 입장
+				children: [{ path: "/urneal", element: <Main /> }],
+			},
+		],
 	},
 	{
 		path: "/",
