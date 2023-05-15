d9f025f1edcca8e10708bc06f773ba6d78067bc2
dbwls387 <dbwls387@naver.com>
2023-05-15 15:24:53 +0900

    Test

--- back/app.js
+++ back/app.js
@@ -143,7 +143,7 @@ io.on("connection", socket => {
     socket.on("start_game", data => {
         io.to(data).emit("start_game_unreal", true);
 
-        console.log(data);
+        console.log("start  ", data);
         // io.to(data).emit("choice_player", 1);
         // io.to(data).emit("main_viewport", 1);
     });
